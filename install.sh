9ed503c172806b4979bb9773e8c64370410c336b
Oskar Schöldström <public@oxy.fi>
2014-03-04 04:06:20 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+
+GIT_ROOT="$(pwd -P 2>/dev/null || command pwd)"
+GIT_ROOT=".."
+while [ ! -e "$GIT_ROOT/.git" ]; do
+  GIT_ROOT="${GIT_ROOT}/.."
+  [[ $GIT_ROOT == "" ]] && exit 1
+done
+
+chmod +x $DIR/hooks/*
+cp -v $DIR/hooks/* $GIT_ROOT/.git/hooks/
