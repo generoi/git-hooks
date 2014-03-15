06181b0dbe9236fee3034f0fc483c441306d12a1
Oskar Schöldström <public@oxy.fi>
2014-03-15 10:55:04 +0100

    Use readlink instead of realpath for mac support

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 GIT_ROOT=".."
 while [ ! -e "$GIT_ROOT/.git" ]; do
   GIT_ROOT="${GIT_ROOT}/.."
-  [[ "$(realpath $GIT_ROOT)" == "/" ]] && exit 1
+  [[ "$(readlink -f $GIT_ROOT)" == "/" ]] && exit 1
 done
 
 chmod +x $DIR/hooks/*
